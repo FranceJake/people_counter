6c8ca476bca9e1ed3f41e410d35ba812eaa4e1f8
FranceJake <108674359+FranceJake@users.noreply.github.com>
2022-08-26 18:53:06 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let saveEl = document.getElementById('save-el');
+let countEl = document.getElementById('counter-el');
+let count = 0;
+
+
+
+
+function increment() {
+    count += 1;
+    countEl.textContent = count; 
+} 
+
+
+function save() {
+    let countStr = count + ' - ';
+    saveEl.textContent += countStr; 
+    console.log(count);
+    countEl.textContent = 0;
+    count = 0;
+}
+
+
+
+console.log("Let's count the number of people entered per interval");
